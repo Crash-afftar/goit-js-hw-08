ddc2e1f93b8cd23805aa86ca3435d77298a4da6f
Crash-afftar <sashakhymenko88@gmail.com>
2023-02-26 15:58:05 +0200

    2602

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,50 +2,41 @@ import throttle from 'lodash.throttle';
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form  textarea'),
-  email: document.querySelector('.feedback-form  input'),
+  email: document.querySelector('input[name="email"]'),
+  message: document.querySelector('textarea[name="message"]'),
 };
 
-const formData = {};
+const { email, message, form } = refs;
 
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 1000));
-refs.email.addEventListener('input', throttle(onTextareaInput, 1000));
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(event) {
-  event.preventDefault();
+let formData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
 
-  event.currentTarget.reset();
-  localStorage.removeItem(LOCAL_STORAGE_KEY, JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
-}
-
-function onTextareaInput(event) {
-  formData.email = refs.email.value;
-  formData.textarea = refs.textarea.value;
+function onFormInput(e) {
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
+  formData[e.target.name] = e.target.value;
 }
 
-function populateTextarea() {
-  const savedMessage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-  
-  if (savedMessage) {
-    refs.textarea.value = savedMessage.textarea;
-    refs.email.value = savedMessage.email;
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  if (!email.value || !message.value) {
+    alert('всі поля повиннібути заповнені');
+  } else {
+    e.target.reset();
+    formData = {};
+    console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
   }
 }
 
-populateTextarea();
+function getValueFromLocalStorage() {
+  //const { email, message } = refs;
+  email.value = formData.email || '';
+  message.value = formData.message || '';
+}
 
-refs.form.style.backgroundColor = '#8C8C8C';
-refs.email.style.border = '2px solid #1D1D1D';
-refs.email.style.borderRadius = '5px';
-refs.email.style.backgroundColor = '#D6E7EA';
-refs.textarea.style.border = '2px solid #1D1D1D';
-refs.textarea.style.borderRadius = '5px';
-refs.textarea.style.backgroundColor = '#D6E7EA';
-document.querySelector('button').style.border = '2px solid #EEF5F6';
-document.querySelector('button').style.borderRadius = '5px';
-document.querySelector('button').style.backgroundColor = '#30575E';
-document.querySelector('button').style.color = '#EEF5F6';
+getValueFromLocalStorage();
